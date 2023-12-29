62c1ddf6c8fb8311723b7cd49dbd0c50ac150855
Minura Dinupriya <mranaweera793@gmail.com>
2023-12-29 13:24:09 +0530

    login form ui done

--- src/main/java/edu/icet/crm/controller/LoginViewController.java
+++ src/main/java/edu/icet/crm/controller/LoginViewController.java
@@ -1,5 +1,28 @@
 package edu.icet.crm.controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class LoginViewController {
 
+    @FXML
+    private JFXTextField txtEmail;
+
+    @FXML
+    private JFXTextField txtPassword;
+
+    @FXML
+    void txtEmailOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtPasswordOnAction(ActionEvent event) {
+
+    }
+
+    public void btnSubmitOnAction(ActionEvent actionEvent) {
+
+    }
 }
