bd6221eefbc766e724f8db43708235aa1d565c74
Minura Dinupriya <mranaweera793@gmail.com>
2023-12-30 14:49:12 +0530

    employee view's sub views set with back and forth navigation

--- src/main/java/edu/icet/crm/controller/EmployeeViewController.java
+++ src/main/java/edu/icet/crm/controller/EmployeeViewController.java
@@ -1,5 +1,45 @@
 package edu.icet.crm.controller;
 
-public class EmployeeViewController {
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class EmployeeViewController {
+    public JFXButton btnItems;
+    public JFXButton btnCustomers;
+    public JFXButton btnOrders;
+    public JFXButton btnLogOut;
+    public BorderPane pane;
+
+    @FXML
+    void logOutBtnOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) pane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/LoginView.fxml"))));
+    }
+
+    public void itemsBtnOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) pane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/ItemsView.fxml"))));
+    }
+
+    public void customerBtnOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) pane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/CustomersView.fxml"))));
+    }
+
+    public void orderBtnOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) pane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/OrdersView.fxml"))));
+    }
+
+    public void placeOrderBtnOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) pane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/PlaceOrderView.fxml"))));
+    }
 }
--- src/main/java/edu/icet/crm/controller/ItemsViewController.java
+++ src/main/java/edu/icet/crm/controller/ItemsViewController.java
@@ -1,5 +1,37 @@
+
 package edu.icet.crm.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ItemsViewController {
+
+    @FXML
+    private BorderPane pane;
+
+    @FXML
+    private JFXButton btnBack;
+
+    @FXML
+    private JFXButton btnLogOut;
+
+    @FXML
+    void backBtnOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) pane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/EmployeeView.fxml"))));
+    }
+
+    @FXML
+    void logOutBtnOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) pane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/LoginView.fxml"))));
+    }
 
 }
