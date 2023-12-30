3c8ad6246f5b1ea4227463598a606fdd6a805eb1
Minura Dinupriya <mranaweera793@gmail.com>
2023-12-30 08:16:09 +0530

    basic files and some navigation set

--- src/main/java/edu/icet/crm/controller/LoginViewController.java
+++ src/main/java/edu/icet/crm/controller/LoginViewController.java
@@ -1,11 +1,18 @@
 package edu.icet.crm.controller;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginViewController {
 
+    public JFXButton btnSubmit;
     @FXML
     private JFXTextField txtEmail;
 
@@ -22,7 +29,8 @@ public class LoginViewController {
 
     }
 
-    public void btnSubmitOnAction(ActionEvent actionEvent) {
-
+    public void btnSubmitOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) btnSubmit.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/OwnerView.fxml"))));
     }
 }
--- src/main/java/edu/icet/crm/controller/OwnerViewController.java
+++ src/main/java/edu/icet/crm/controller/OwnerViewController.java
@@ -1,5 +1,56 @@
 package edu.icet.crm.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class OwnerViewController {
 
+    public JFXButton btnLogOut;
+    @FXML
+    private JFXButton btnEmployee;
+
+    @FXML
+    private JFXButton btnSalesReports;
+
+    @FXML
+    private JFXButton btnOrderReports;
+
+    @FXML
+    private JFXButton btnCustomerReports;
+
+    @FXML
+    void btnCustomerReportsOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) btnSalesReports.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/SalesReportsView.fxml"))));
+    }
+
+    @FXML
+    void btnEmployeesOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) btnEmployee.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/EmployeesView.fxml"))));
+    }
+
+    @FXML
+    void btnOrderReportsOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) btnOrderReports.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/OrderReportsView.fxml"))));
+    }
+
+    @FXML
+    void btnSalesReportsOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) btnSalesReports.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/SalesReportsView.fxml"))));
+    }
+
+
+    public void btnLogOutOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) btnLogOut.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/MainView.fxml"))));
+    }
 }
