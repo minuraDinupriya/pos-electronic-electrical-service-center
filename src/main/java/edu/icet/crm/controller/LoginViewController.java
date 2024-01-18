698c68fb956f46b02f998f8b3d5d1d5e3afa0718
Minura Dinupriya <mranaweera793@gmail.com>
2024-01-18 13:09:09 +0530

    login view, submit btnOnAction done with password validation using md5

--- src/main/java/edu/icet/crm/controller/LoginViewController.java
+++ src/main/java/edu/icet/crm/controller/LoginViewController.java
@@ -2,17 +2,30 @@ package edu.icet.crm.controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import edu.icet.crm.bo.BoFactory;
+import edu.icet.crm.bo.BoType;
+import edu.icet.crm.bo.custom.UserViewBo;
+import edu.icet.crm.dto.UserDto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.scene.control.Alert;
 
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class LoginViewController {
 
-    public JFXButton btnSubmit;
+    private final UserViewBo userViewBo = BoFactory.getInstance().getBo(BoType.USERS_VIEW_BO);
+    public AnchorPane pane;
+
+    @FXML
+    private JFXButton btnSubmit;
+
     @FXML
     private JFXTextField txtEmail;
 
@@ -20,17 +33,56 @@ public class LoginViewController {
     private JFXTextField txtPassword;
 
     @FXML
-    void txtEmailOnAction(ActionEvent event) {
+    public void btnSubmitOnAction(ActionEvent actionEvent) throws IOException {
+        String userName = txtEmail.getText();
+        String enteredPassword = txtPassword.getText();
 
+        UserDto userDto = userViewBo.getUsers().stream()
+                .filter(user -> user.getUserName().equals(userName))
+                .findFirst()
+                .orElse(null);
+
+        if (userDto != null) {
+            String storedPassword = userDto.getPassword();
+
+            if (storedPassword.equals(hashPassword(enteredPassword))) {
+                Stage stage = (Stage) pane.getScene().getWindow();
+                stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/OwnerView.fxml"))));
+            } else {
+                showAlert("Login Failed", "Invalid username or password.");
+            }
+        } else {
+            showAlert("Login Failed", "Invalid username or password.");
+        }
+    }
+
+    private void showAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    private String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] hashedBytes = md.digest(password.getBytes());
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (byte b : hashedBytes) {
+                stringBuilder.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+            }
+
+            return stringBuilder.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @FXML
-    void txtPasswordOnAction(ActionEvent event) {
+    public void forgottenPasswordBtnOnAction(ActionEvent actionEvent) {
 
-    }
-
-    public void btnSubmitOnAction(ActionEvent actionEvent) throws IOException {
-        Stage stage = (Stage) btnSubmit.getScene().getWindow();
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/OwnerView.fxml"))));
     }
 }
